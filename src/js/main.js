3c851ea19484f0fbee8f0a4983a78fe6f4e20811
estebangccoder <estebangccoder@gmail.com>
2024-04-28 00:17:20 -0500

    se finaliza la informacion de la clase

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,80 @@
-const fecha = new Date();
+// const fecha = new Date();
 
-console.log(fecha);
+// console.log(fecha);
 
-const juanjoBD=new Date(2005,8,1);
-console.log(juanjoBD);
+// const juanjoBD=new Date(2005,8,1);
+// console.log(juanjoBD);
 
-const juanjoBDString=new Date("09-01-2005");
+// const juanjoBDString=new Date("09-01-2005");
 
-console.log(juanjoBDString);
+// console.log(juanjoBDString);
 
-//Metodos de fecha
+// //Metodos de fecha
 
-const dia= fecha.getDate();
-console.log(dia);
+// const dia= fecha.getDate();
+// console.log(dia);
 
-const mont = fecha.getMonth();
-console.log(mont);
+// const mont = fecha.getMonth();
+// console.log(mont);
 
-const fullyear = fecha.getFullYear();
-console.log(fullyear);
+// const fullyear = fecha.getFullYear();
+// console.log(fullyear);
 
-const year = fecha.getYear();
-console.log(year);
+// const year = fecha.getYear();
+// console.log(year);
 
-const day= fecha.getDay();
-console.log(day); //devuelve en numero del dia comenzado en domingo=0
+// const day= fecha.getDay();
+// console.log(day); //devuelve en numero del dia comenzado en domingo=0
 
-const hora = fecha.getHours();
-console.log(hora);
+// const hora = fecha.getHours();
+// console.log(hora);
 
-const minutos = fecha.getMinutes();
-console.log(minutos);
+// const minutos = fecha.getMinutes();
+// console.log(minutos);
 
-const segundos = fecha.getSeconds();
-console.log(segundos);
+// const segundos = fecha.getSeconds();
+// console.log(segundos);
 
-const milisegundos = fecha.getMilliseconds();
-console.log(milisegundos);
+// const milisegundos = fecha.getMilliseconds();
+// console.log(milisegundos);
 
-const fechaUsuario =new Date();
+// const fechaUsuario =new Date();
 
-const usuario=new Date(prompt("Ingrese una fecha: ", "MM/DD/YY"))
+// const usuario=new Date(prompt("Ingrese una fecha: ", "MM/DD/YY"))
 
-console.log(usuario);
+// console.log(usuario);
 
-console.log(fecha.getTime());
-const dias5= fecha.setDate(fecha.getDate()+5)
-console.log(new Date(dias5))
-console.log(fecha)
+// console.log(fecha.getTime());
+// const dias5= fecha.setDate(fecha.getDate()+5)
+// console.log(new Date(dias5))
+// console.log(fecha)
+
+// const diasPrestamo = parseInt(prompt("Ingrese la cantidad de dias del prestamo: "));
+
+// const fechaDevolucion = new Date();
+
+// fechaDevolucion.setDate(fechaDevolucion.getDate() + diasPrestamo)
+
+// console.log(fechaDevolucion);
 
 
+const fechaBD = new Date (prompt("Ingrese su fecha de nacimiento","MM/DD/YYYY"))
+const fechaActual = new Date()
+
+//const anios = (fechaActual.getFullYear()-fechaBD.getFullYear());
+
+const anios2 = Math.floor((fechaActual - fechaBD)/(1000*3600*24*365))
+
+console.log(anios2);
+
+// Ejercicio 2
+// console.log("Ejercicio 2");
+
+// const fechaCumpleanosString = "02/11/2024"//prompt("Ingrese la fecha de cumpleaños: " , "MM/DD/YYYY");
+// const fechaCumpleanos = new Date(fechaCumpleanosString);
+
+// console.log(fechaCumpleanos);
+ 
+// const fecha  = new Date()
+// const conteo = (fechaCumpleanos - fecha)/(1000*3600*24);
+// console.log(conteo);
